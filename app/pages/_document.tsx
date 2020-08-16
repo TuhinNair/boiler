5e3956bb8208189e3f1fd9d1a01cf6d8914634e9
Tuhin Nair <tuhinnair444@gmail.com>
2020-08-17 06:57:11 +0700

    configure linter

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,22 +1,22 @@
-import Document, {Head, Html, Main, NextScript} from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
 class MyDocument extends Document {
-    public render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    <meta charSet="utf-8" />
-                    <meta name="google" content="notranslate" />
-                    <meta name="theme-color" content="#303030" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  public render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="google" content="notranslate" />
+          <meta name="theme-color" content="#303030" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
