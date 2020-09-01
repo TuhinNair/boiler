3631d01c4da4f438f6bcba5bbb5fcadf54f6d266
Tuhin Nair <tuhinnair444@gmail.com>
2020-09-01 19:11:01 +0700

    Add cookie configuration

--- api/server/server.ts
+++ api/server/server.ts
@@ -35,6 +35,11 @@ const sessionOptions = {
     }),
     resave: false,
     saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 14 * 24 * 60 * 60 * 1000,
+        secure: false,
+    }
 };
 
 server.use(session(sessionOptions));
